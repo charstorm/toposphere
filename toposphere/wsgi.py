4d42d6334d40336a840161a434be92f2587a329c
Vinay Krishnan <nk.vinay@zohomail.in>
2026-02-15 16:46:28 +0530

    Rename project from personal_space to toposphere

--- toposphere/wsgi.py
+++ toposphere/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for personal_space project.
+WSGI config for toposphere project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_space.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toposphere.settings')
 
 application = get_wsgi_application()
